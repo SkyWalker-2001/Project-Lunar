c5fe038c8963b467c611f88cedc43ae73c08bc62
gagan <126860277+SkyWalker-2001@users.noreply.github.com>
2024-10-18 13:37:16 +0530

    Section 4 Basic Input Very Basic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
+    public Rigidbody2D _rigidbody2D;
 
+    [Header("Move Info")]
     public float moveSpeed;
     public float jumpForce;
+    public bool runBegin = false;
 
-    public Rigidbody2D _rigidbody2D;
+    [Header("Collision Info")]
+
+    public float groundCheck; 
+    private bool isGrounded;
+    public LayerMask whatIsGround;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +30,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rigidbody2D.velocity = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
+        if (runBegin)
+        {
+            _rigidbody2D.velocity = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
+        }
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        CheckCollision();
+
+        CheckInput();
+    }
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheck, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            runBegin = true;
+        }
+
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
         }
+    }
+
+    public void OnDrawGizmos(){
+        Gizmos.DrawLine(transform.position,new Vector2(transform.position.x, transform.position.y - groundCheck));
     }
 }
